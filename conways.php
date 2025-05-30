56e2f5659413ce6d7d3e1c083809ae165a56ea93
Damien Bevins <damien.bevins@outlook.com>
2025-05-30 06:04:56 -0600

    Add `Cell` instances that can be shown in the window, and hovering turns them green

--- conways.php
+++ conways.php
@@ -4,32 +4,140 @@
 
 include_once("ffi.php");
 
+class Cell {
+	public bool $alive = false;
+	public bool $hover = false;
+	public Rectangle $renderRect;
+	public int $topLeft;
+	public int $topMiddle;
+	public int $topRight;
+	public int $left;
+	public int $right;
+	public int $bottomLeft;
+	public int $bottomMiddle;
+	public int $bottomRight;
+
+	public function __construct(int $x, int $y, Size $size) {
+		$this->renderRect = new Rectangle($x, $y, $size->width, $size->height);	
+	}
+}
+
 class Game {
 	private Color $background;
-	private Raylib $raylib;	
+	private Color $cellColor;
+	private Color $hoverColor;
+	private Raylib $raylib;
+	private int $horizontalCount;
+	private int $verticalCount;
+	private array $cells;
 
-	function __construct() {
+	function __construct(int $gridWidth, int $gridHeight, int $horizontalCount, int $verticalCount) {
+		$width = 1280;
+		$height = 800;
+		$offsetX = ($width / 2) - ($gridWidth / 2);
+		$offsetY = ($height / 2) - ($gridHeight / 2);
+		$cellSize = new Size($gridWidth / $horizontalCount, $gridHeight / $verticalCount);
+		$cells = [];
+
+		$this->horizontalCount = $horizontalCount;
+		$this->verticalCount = $verticalCount;
+
+		$this->cellColor = new Color(0x88, 0x08, 0x08, 0xFF);
+		$this->hoverColor = new Color(0x08, 0x88, 0x08, 0xFF);
 		$this->background = new Color(0x28, 0x28, 0x28, 0xFF);
 		$this->raylib = new Raylib();
 
-		$this->raylib->initWindow(1280, 800, "Raylib + PHP Demo");
+		$this->raylib->initWindow($width, $height, "Raylib + PHP Demo");
 		$this->raylib->setTargetFPS(60);
+
+		for ($y = 0; $y < $verticalCount; $y++) {
+			for ($x = 0; $x < $horizontalCount; $x++) {
+				$cellX = $cellSize->width * $x + $offsetX;
+				$cellY = $cellSize->height * $y + $offsetY;
+				$cell = new Cell($cellX, $cellY, $cellSize);
+
+				$cell->topLeft = $this->getIndex($x - 1, $y - 1);
+				$cell->topMiddle = $this->getIndex($x, $y - 1);
+				$cell->topRight = $this->getIndex($x + 1, $y - 1);
+				$cell->left = $this->getIndex($x - 1, $y);
+				$cell->right = $this->getIndex($x + 1, $y);
+				$cell->bottomLeft = $this->getIndex($x - 1, $y + 1);
+				$cell->bottomMiddle = $this->getIndex($x, $y + 1);
+				$cell->bottomRight = $this->getIndex($x + 1, $y + 1);
+
+				array_push($cells, $cell);
+			}
+		}
+
+		$this->cells = $cells;
 	}
 
 	function __destruct() {
 		$this->raylib->closeWindow();
 	}
 
+	function getIndex(int $x, int $y): int {
+		if ($x < 0 || $this->horizontalCount <= $x || $y < 0 || $this->verticalCount <= $y) {
+			return -1;
+		}
+
+		return $y * $this->horizontalCount + $x;
+	}
+
+	function isAlive(int $index): bool {
+		if ($index == -1) {
+			return false;
+		}
+
+		return $this->cells[$index]->alive;
+	}
+
+	function updateCells() {
+		$position = $this->raylib->getMousePosition();
+
+		for ($y = 0; $y < $this->verticalCount; $y++) {
+			for ($x = 0; $x < $this->horizontalCount; $x++) {
+				$index = $this->getIndex($x, $y);
+				$cell = $this->cells[$index];
+
+				$aliveCount = (int)$this->isAlive($cell->topLeft)
+					+ (int)$this->isAlive($cell->topMiddle)
+					+ (int)$this->isAlive($cell->topRight)
+					+ (int)$this->isAlive($cell->left)
+					+ (int)$this->isAlive($cell->right)
+					+ (int)$this->isAlive($cell->bottomLeft)
+					+ (int)$this->isAlive($cell->bottomMiddle)
+					+ (int)$this->isAlive($cell->bottomRight);
+
+				$cell->alive = 1 < $aliveCount && $aliveCount < 4;
+				$cell->hover = $this->raylib->checkCollisionPointRec($position, $cell->renderRect);
+			}
+		}
+	}
+
+	function renderCells() {
+		foreach($this->cells as $cell) {
+			$color = $cell->hover ? $this->hoverColor : $this->cellColor;
+
+			$this->raylib->drawRectangleRec($cell->renderRect, $color);
+		}
+	}
+
 	function run() {
 		while (!$this->raylib->windowShouldClose()) {
+			$this->updateCells();
+
 			$this->raylib->beginDrawing();
 			$this->raylib->clearBackground($this->background);
+
+			$this->renderCells();
+
 			$this->raylib->endDrawing();
 		}
 	}
 }
 
-$game = new Game();
+$game = new Game(600, 600, 10, 10);
 
 $game->run();
 
--- ffi.php
+++ ffi.php
@@ -1,5 +1,43 @@
 <?php
 
+const ONE = new Vector2(1, 1);
+const ZERO = new Vector2(0, 0);
+const BLACK = new Color(0, 0, 0, 255);
+
+class Vector2 {
+	public int $x;
+	public int $y;
+
+	public function __construct(int $x, int $y) {
+		$this->x = $x;
+		$this->y = $y;
+	}
+}
+
+class Size {
+	public int $width;
+	public int $height;
+
+	public function __construct(int $width, int $height) {
+		$this->width = $width;
+		$this->height = $height;
+	}
+}
+
+class Rectangle {
+	public int $x;
+	public int $y;
+	public int $width;
+	public int $height;
+
+	public function __construct(int $x, int $y, int $width, int $height) {
+		$this->x = $x;
+		$this->y = $y;
+		$this->width = $width;
+		$this->height = $height;
+	}
+}
+
 class Color {
 	public int $r {
 		get {
@@ -45,11 +83,12 @@ class Color {
 	}
 }
 
-const BLACK = new Color(0, 0, 0, 255);
-
 class Raylib {
 	private \FFI $ffi;
-	private \FFI\CType $color_type;
+	private \FFI\CType $vector2Type;
+	private \FFI\CType $sizeType;
+	private \FFI\CType $rectangleType;
+	private \FFI\CType $colorType;
 
 	function __construct() {
 		$raylib = \FFI::cdef("
@@ -60,6 +99,23 @@ class Raylib {
 				unsigned char a;
 			} Color;
 
+			typedef struct Vector2 {
+				float x;
+				float y;
+			} Vector2;
+
+			typedef struct Size {
+				float width;
+				float height;
+			} Size;
+
+			typedef struct Rectangle {
+				float x;
+				float y;
+				float width;
+				float height;
+			} Rectangle;
+
 			void InitWindow(int width, int height, const char *title);
 			void SetTargetFPS(int fps);
 			bool WindowShouldClose(void);
@@ -67,25 +123,61 @@ class Raylib {
 			void ClearBackground(Color color);
 			void EndDrawing(void);
 			void CloseWindow(void);
-			", "libraylib.so");
+
+			void DrawRectangleRec(Rectangle rec, Color color);
+			Vector2 GetMousePosition(void);
+			bool CheckCollisionPointRec(Vector2 point, Rectangle rec);
+		", "libraylib.so");
 
 		if (!isset($raylib)) {
 			die("could not load raylib.h");
 		}
 
 		$this->ffi = $raylib;
-		$this->color_type = $this->ffi->type("struct Color");
+		$this->colorType = $this->ffi->type("struct Color");
+		$this->vector2Type = $this->ffi->type("struct Vector2");
+		$this->sizeType = $this->ffi->type("struct Size");
+		$this->rectangleType = $this->ffi->type("struct Rectangle");
+	}
+
+	function convertVector2(Vector2 $vector2): \FFI\CData {
+		$convertedVector2 = $this->ffi->new($this->vector2Type);
+
+		$convertedVector2->x = $vector2->x;
+		$convertedVector2->y = $vector2->y;
+
+		return $convertedVector2;
+	}
+
+	function convertSize(Size $size): \FFI\CData {
+		$convertedSize = $this->ffi->new($this->sizeType);
+
+		$convertedSize->width = $size->width;
+		$convertedSize->height = $size->height;
+
+		return $convertedSize;
+	}
+
+	function convertRectangle(Rectangle $rect): \FFI\CData {
+		$convertedRect = $this->ffi->new($this->rectangleType);
+
+		$convertedRect->x = $rect->x;
+		$convertedRect->y = $rect->y;
+		$convertedRect->width = $rect->width;
+		$convertedRect->height = $rect->height;
+
+		return $convertedRect;
 	}
 
 	function convertColor(Color $color): \FFI\CData {
-		$converted_color = $this->ffi->new($this->color_type);
+		$convertedColor = $this->ffi->new($this->colorType);
 
-		$converted_color->r = $color->r;
-		$converted_color->g = $color->g;
-		$converted_color->b = $color->b;
-		$converted_color->a = $color->a;
+		$convertedColor->r = $color->r;
+		$convertedColor->g = $color->g;
+		$convertedColor->b = $color->b;
+		$convertedColor->a = $color->a;
 
-		return $converted_color;
+		return $convertedColor;
 	}
 
 	public function initWindow(int $width, int $height, string $title) {
@@ -105,9 +197,9 @@ class Raylib {
 	}
 
 	public function clearBackground(Color $color) {
-		$converted_color = $this->convertColor($color);
+		$convertedColor = $this->convertColor($color);
 
-		$this->ffi->ClearBackground($converted_color);
+		$this->ffi->ClearBackground($convertedColor);
 	}
 
 	public function endDrawing() {
@@ -116,6 +208,26 @@ class Raylib {
 
 	public function closeWindow() {
 		$this->ffi->CloseWindow();
+	}
+
+	public function drawRectangleRec(Rectangle $rectangle, Color $color) {
+		$convertedRectangle = $this->convertRectangle($rectangle);
+		$convertedColor = $this->convertColor($color);
+
+		$this->ffi->DrawRectangleRec($convertedRectangle, $convertedColor);
+	}
+
+	public function getMousePosition(): Vector2 {
+		$position = $this->ffi->GetMousePosition();
+
+		return new Vector2($position->x, $position->y);
+	}
+
+	public function checkCollisionPointRec(Vector2 $position, Rectangle $rectangle): bool {
+		$convertedVector2 = $this->convertVector2($position);
+		$convertedRectangle = $this->convertRectangle($rectangle);
+
+		return $this->ffi->CheckCollisionPointRec($convertedVector2, $convertedRectangle);
 	}
 }
 
